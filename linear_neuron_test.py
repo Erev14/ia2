fdc76c6b129af1c40d163139450f7bde7345f13e
Erev14 <erevjoach14@gmail.com>
2022-09-20 22:43:15 -0500

    chore: fix linter issues

--- linear_neuron_test.py
+++ linear_neuron_test.py
@@ -1,26 +1,26 @@
 import numpy as np
 import matplotlib.pyplot as plt 
-from linear_neuron import Linear_Neuron
+from linear_neuron import LinearNeuron
 
 # p = 200
 # x = -1 + 2 * np.random.rand(p).reshape(1,-1)
 # y = -18 * x + 6 + 3 * np.random.rand(p)
 # plt.plot(x,y, 'b.')
 
-# neuron = Linear_Neuron(1, 0.1)
+# neuron = LinearNeuron(1, 0.1)
 # neuron.fit(x, y, solver='BGD')
 # xn = np.array([[-1, 1]])
 # plt.plot(xn.ravel(), neuron.predict(xn), '--r')
 # plt.show()
 
-# neuron = Linear_Neuron(1, 0.1)
+# neuron = LinearNeuron(1, 0.1)
 # neuron.fit(x, y, solver='SGD')
 # xn = np.array([[-1, 1]])
 # plt.plot(x,y, 'b.')
 # plt.plot(xn.ravel(), neuron.predict(xn), '--r')
 # plt.show()
 
-# neuron = Linear_Neuron(1, 0.1)
+# neuron = LinearNeuron(1, 0.1)
 # neuron.fit(x, y, solver='Pseudo-Inverse')
 # xn = np.array([[-1, 1]])
 # plt.plot(x,y, 'b.')
@@ -34,7 +34,7 @@ x = -1 + 2 * np.random.rand(p).reshape(1,-1)
 y = -18 * x + 6 + 3 * np.random.rand(p)
 plt.plot(x,y, 'b.')
 
-neuron = Linear_Neuron(1, 0.1)
+neuron = LinearNeuron(1, 0.1)
 neuron.fit(x, y, batch_size=10)
 xn = np.array([[-1, 1]])
 plt.plot(x,y, 'b.')
